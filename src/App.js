5993aca795d3353f2810923421849843bca6c48d
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-01-14 18:41:53 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route } from "react-router-dom";
+
+import AllMeetupPage from "./pages/AllMeetUps";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Route path="/" exact>
+        <AllMeetupPage />
+      </Route>
+      <Route path="/new-meetup">
+        <NewMeetupPage />
+      </Route>
+      <Route path="/favorites">
+        <FavoritesPage />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
