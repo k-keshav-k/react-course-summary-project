ecbca566e6126e3bb2da55a13f7ca5366d78e41d
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-01-16 11:10:00 +0530

    updated

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -13,6 +13,17 @@ function NewMeetupForm() {
           <label htmlFor="image">Meetup Image</label>
           <input type="url" required id="image" />
         </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Image</label>
+          <textarea id='description' required rows='5'></textarea>
+        </div>
+        <div className={classes.actions}>
+            <button>Add Meetup</button>
+        </div>
       </form>
     </Card>
   );
