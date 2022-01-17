8b2c879da1a31a482d61d11ddc720df7c0381f70
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-01-17 13:11:51 +0530

    updated

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -8,6 +8,20 @@ const FavoritesContext = createContext({
 function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
 
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorite) => {
+      return prevUserFavorite.concat(favoriteMeetup);
+    });
+  }
+
+  function removeFavoriteHandler(meetupId) {
+    setUserFavorites(prevUserFavorite => {
+      return prevUserFavorite.filter(meetup => meetup.id !== meetupId);
+    })
+  }
+
+  function itemIsFavoriteHandler() {}
+
   const context = {
       favorites: userFavorites,
       totalFavorites: userFavorites.length,
