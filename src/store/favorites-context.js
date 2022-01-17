e5e18d8c8332a8ce4a6a4934c701376b16cf5dc4
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2022-01-17 13:19:59 +0530

    updated

--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -3,9 +3,12 @@ import { createContext, useState } from "react";
 const FavoritesContext = createContext({
   favorites: [],
   totalFavorites: 0,
+  addFavorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavorite: (meetupId) => {}
 });
 
-function FavoritesContextProvider(props) {
+export function FavoritesContextProvider(props) {
   const [userFavorites, setUserFavorites] = useState([]);
 
   function addFavoriteHandler(favoriteMeetup) {
@@ -20,14 +23,21 @@ function FavoritesContextProvider(props) {
     })
   }
 
-  function itemIsFavoriteHandler() {}
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some(meetup => meetup.id === meetupId);
+  }
 
   const context = {
       favorites: userFavorites,
       totalFavorites: userFavorites.length,
+      addFavorite: addFavoriteHandler,
+      removeFavorite: removeFavoriteHandler,
+      itemIsFavorite: itemIsFavoriteHandler
   };  
 
   return (
     <FavoritesContext.Provider value={context}>{props.children}</FavoritesContext.Provider>
   );
 }
+
+export default FavoritesContext;
